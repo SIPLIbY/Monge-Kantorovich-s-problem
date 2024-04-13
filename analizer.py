28ba2acec846f3b7c3d713a26f51144f6d4c7b21
0xUSDTKING <0xUSDTKING@gmail.com>
2024-04-13 17:01:28 +0700

    refactoring

--- analizer.py
+++ analizer.py
@@ -47,7 +47,7 @@ def calculate_loss(array, marginality):
 
 
 
-EPOCHS = 1000
+EPOCHS = 10
 # AMOUNT_OF_PRODUCTS = 1
 # AMOUNT_OF_FACTORIES = 1
 # AMOUNT_OF_STOCKS = 1
@@ -66,11 +66,11 @@ PUNISHMENT_STOCK = 0.01
 lambda_param = 20
 
 #возможные значения CONFIDENCE_LEVEL_SHOP and CONFIDENCE_LEVEL_STOCK
-values_confidence = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
-
+# values_confidence = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+values_confidence = [0.5, 0.95]
 
 # Возможные значения параметров
-values = [1, 10]
+values = [1, 2]
 
 # remains_shop_on_each_iteration, remain_stock_on_each_iteration, objective_values, lost_products_shop, lost_products_stock = solution(EPOCHS, AMOUNT_OF_PRODUCTS, AMOUNT_OF_FACTORIES, AMOUNT_OF_STOCKS, AMOUNT_OF_SHOPS, CONFIDENCE_LEVEL_SHOP, CONFIDENCE_LEVEL_STOCK, lambda_param)
 # #plot objective values
@@ -89,13 +89,15 @@ for products in values:
     for factories in values:
         for stock in values:
             for shop in values:
+                df = pd.DataFrame()
+
                 for CONFIDENCE_LEVEL_SHOP in values_confidence:
                     for CONFIDENCE_LEVEL_STOCK in values_confidence:
                         
                         remains_shop_on_each_iteration, remain_stock_on_each_iteration, objective_values, lost_products_shop, lost_products_stock = solution(EPOCHS, products, factories, stock, shop, CONFIDENCE_LEVEL_SHOP, CONFIDENCE_LEVEL_STOCK, lambda_param)
                         
                         #create folder for saving results
-                        path = f"{path_to_folder}/products_{products}/factories_{factories}/stock_{stock}/shop_{shop}/confidence_shop_{CONFIDENCE_LEVEL_SHOP}/confidence_stock_{CONFIDENCE_LEVEL_STOCK}"
+                        path = f"{path_to_folder}/products_{products}/factories_{factories}/stock_{stock}/shop_{shop}"
                         if not os.path.exists(path):
                             os.makedirs(path)
                         
@@ -109,8 +111,8 @@ for products in values:
                         for epoch in range(EPOCHS - 1):
                             loss_1 = MARGINALITY * lost_products_shop[epoch]
                             loss_2 = MARGINALITY * lost_products_stock[epoch]
-                            loss_3 = PUNISHMENT_SHOP * sum(remains_shop_on_each_iteration[epoch, i, l] for i, l in itertools.product(range(AMOUNT_OF_PRODUCTS), range(AMOUNT_OF_SHOPS)))
-                            loss_4 = PUNISHMENT_STOCK * sum(remain_stock_on_each_iteration[epoch, i, l] for i, l in itertools.product(range(AMOUNT_OF_PRODUCTS), range(AMOUNT_OF_STOCKS)))
+                            loss_3 = PUNISHMENT_SHOP * sum(remains_shop_on_each_iteration[epoch, i, l] for i, l in itertools.product(range(products), range(shop)))
+                            loss_4 = PUNISHMENT_STOCK * sum(remain_stock_on_each_iteration[epoch, i, l] for i, l in itertools.product(range(products), range(stock)))
 
                             losses.append(loss_1 + loss_2 + loss_3 + loss_4)
 
@@ -125,7 +127,11 @@ for products in values:
                         variation_coefficient_loss = np.sqrt(variance_loss) / mean_loss
 
                         #save results of test results to csv
-                        df = pd.DataFrame({
+                        #add results to dataframe
+                        #genereate one csv file appending to df
+                        temp = pd.DataFrame({
+                            "confidence_level_shop": [CONFIDENCE_LEVEL_SHOP],
+                            "confidence_level_stock": [CONFIDENCE_LEVEL_STOCK],
                             "variance_objective": [variance],
                             "mean_objective": [mean],
                             "variation_coefficient_objective": [variation_coefficient_objective],
@@ -138,24 +144,24 @@ for products in values:
                             "variation_coefficient_loss": [variation_coefficient_loss],
                             "integral_loss": [integral_loss]
                         })
-                        #create and save graphics with title
+                        df = pd.concat([df, temp], ignore_index=True)
+                        
+                        #plot objective values
                         plt.plot(objective_values)
-                        plt.title(f"OBJECTIVE_products_{products}_factories_{factories}_stock_{stock}_shop_{shop}")
-                        plt.savefig(f"{path}/objective_values.png")
+                        plt.savefig(f"{path}/objective_shop_{CONFIDENCE_LEVEL_SHOP}__stock_{CONFIDENCE_LEVEL_STOCK}.png")
                         plt.close()
 
-                        #create and save graphics with title for losses
+                        #plot losses
                         plt.plot(losses)
-                        plt.title(f"LOSS_products_{products}_factories_{factories}_stock_{stock}_shop_{shop}")
-                        plt.savefig(f"{path}/loss_values.png")
-
+                        plt.savefig(f"{path}/loss_shop_{CONFIDENCE_LEVEL_SHOP}__stock_{CONFIDENCE_LEVEL_STOCK}.png")
                         plt.close()
-                        df.to_csv(f"{path}/test_results.csv", index=False)
-
+                        
                         #print progress
                         # print(f"products_{products}_factories_{factories}_stock_{stock}_shop_{shop} is done")
                         counter += 1
-                        # print(f'THATS ONLY {(counter/total_iterations) * 100}%')
+                        print(f'THATS ONLY {(counter/total_iterations) * 100}%')
+                
+                df.to_csv(f"{path}/test_results.csv", index=False)
                 
                 
                 
